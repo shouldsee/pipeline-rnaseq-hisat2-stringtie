f93ad2342fca5d8ffe8834c66f3dac3319abd72d
shouldsee <shouldsee@github.com>
2020-03-17 00:24:56 +0800

    add docstring

--- pipeline_rnaseq_hisat2_stringtie.py
+++ pipeline_rnaseq_hisat2_stringtie.py
@@ -1,3 +1,30 @@
+__doc__ = '''
+Comment: This is the default pair-end RNA-Seq pipeline with hisat2 during Feng's time at SLCU.
+Author: Feng Geng
+Last Modified: 2020/03/16
+Example Usage: 
+
+	BIN=python3
+	PACKAGE=pipeline_rnaseq_hisat2_stringtie@https://github.com/shouldsee/pipeline-rnaseq-hisat2-stringtie/tarball/master
+
+	## install dependencies
+	$BIN -m pip install spiper@https://github.com/shouldsee/spiper/tarball/master --user --upgrade
+	curl -sL https://raw.githubusercontent.com/shouldsee/spiper/master/scripts/install_singular.sh | bash -s $HOME/.local
+
+	## test example runs
+	$BIN -m spiper get_changed_files $PACKAGE TOPLEVEL:test_job _temp_build/root --plain
+	$BIN -m spiper run               $PACKAGE TOPLEVEL:test_job _temp_build/root
+
+	$BIN -m spiper run $PACKAGE TOPLEVEL:workflow \
+		$HOME/run_temp/0306.201RS6/root \
+		$HOME/_hisat2_cache/Bd21_v3-1 \
+		$HOME/ref/Bd21_v3-1/assembly/Bdistachyon_314_v3.0.fa \
+		$HOME/ref/Bd21_v3-1/annotation/Bdistachyon_314_v3.1.gene_exons.gtf \
+		$HOME/envs/upGeo/brachy-meta/rawfile.workdir/201RS6/combined_valid_fastq/R1_001.fastq.gz \
+		$HOME/envs/upGeo/brachy-meta/rawfile.workdir/201RS6/combined_valid_fastq/R2_001.fastq.gz \
+		8
+
+'''
 from spiper.types import File,TempFile, Prefix, Default
 from spiper.types import job_result
 from spiper.types import Depend
@@ -6,7 +33,7 @@ from spiper.runner import list_flatten_strict, job_from_func
 from spiper.shell import LoggedSingularityCommandList, LoggedShellCommand, LoggedSingularityCommand
 from path import Path
 import spiper
-assert spiper.VERSION >= '0.0.5',spiper.VERSION
+assert spiper.VERSION >= '0.0.8',spiper.VERSION
 
 from spiper.types import Concat
 from spiper.types import Flow
@@ -281,7 +308,10 @@ def LinkEvent(self,
 	#### One can also use directly move the output file, but this would break the upstream integrity 
 	#### and is hence not recommended
 	'''
-	# os.link()
+	output = prefix
+	haso = os.path.exists(output)
+	hasi = os.path.exists(input)
+
 	shutil.copy2(input, self.prefix+'.temp')
 	shutil.move(self.prefix +'.temp', self.prefix)
 
