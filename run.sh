9cbea5ffa15a66949b2009c506aae96f13215ad7
shouldsee <shouldsee@github.com>
2020-03-16 14:49:20 +0800

    file://

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 BIN=${1:-python3}
 # $BIN -m spiper get_changed_files pipeline_rnaseq_hisat2_stringtie@file://$PWD TOPLEVEL:main _temp_build/root --plain
 set -e
-$BIN -m spiper get_changed_files $PWD pipeline_rnaseq_hisat2_stringtie:main _temp_build/root --plain
-$BIN -m spiper run $PWD pipeline_rnaseq_hisat2_stringtie:main _temp_build/root
+PACKAGE=pipeline_rnaseq_hisat2_stringtie@file://$PWD
+$BIN -m spiper get_changed_files $PACKAGE TOPLEVEL:test_job _temp_build/root --plain
+$BIN -m spiper run               $PACKAGE TOPLEVEL:test_job _temp_build/root
 
